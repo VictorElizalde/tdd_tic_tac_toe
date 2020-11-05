405a78f297c5bc37f92f3a531396b153f6b9c233
Victor Elizalde <vic-bull@hotmail.com>
2020-11-05 12:41:36 -0600

    Gets board rows

--- lib/board.rb
+++ lib/board.rb
@@ -4,4 +4,8 @@ class Board
   def initialize()
     @board = ['0', '1', '2', '3', '4', '5', '6', '7', '8']
   end
+
+  def rows
+    board.each_slice(3).to_a
+  end
 end
--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -6,4 +6,8 @@ describe Board do
   it "initializes board" do
     expect(board.board).to eq(['0', '1', '2', '3', '4', '5', '6', '7', '8'])
   end
+
+  it "gets the rows" do
+    expect(board.rows).to eq([["0", "1", "2"], ["3", "4", "5"], ["6", "7", "8"]])
+  end
 end

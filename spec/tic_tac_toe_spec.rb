9b97d72ab9bf25e8a4e2bd30691fd24c2e3f0ea4
Victor Elizalde <vic-bull@hotmail.com>
2020-11-05 12:40:05 -0600

    Initialize board

---
+++ spec/tic_tac_toe_spec.rb
@@ -0,0 +1,9 @@
+require 'board.rb'
+
+describe Board do
+  let(:board) { Board.new }
+
+  it "initializes board" do
+    expect(board.board).to eq(['0', '1', '2', '3', '4', '5', '6', '7', '8'])
+  end
+end
